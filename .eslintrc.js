5e9b1a3922667197c22bf325c3ab2828e6ea6c0f
gy134340 <guoyang134340@163.com>
2017-03-26 23:57:36 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "globals": {
+        "Vue": true,
+        "VueRouter": true,
+        "app": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+         // "no-console": "off",
+        "no-unused-vars": ["off", { "caughtErrors": "none" }]
+    }
+};
